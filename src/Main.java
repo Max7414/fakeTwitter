9689b646b1c271e6b036bf9691ae654421f4825b
guomengxun <komax0109@gmail.com>
2024-05-28 15:10:16 +0800

    ver 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,93 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        Twitter twitter = new Twitter();
-        twitter.postTweet(1,5);
-        twitter.postTweet(2,3);
-        twitter.postTweet(1,101);
-        twitter.postTweet(2,13);
-        twitter.postTweet(2,10);
-        twitter.postTweet(1,2);
-        twitter.postTweet(1,94);
-        twitter.postTweet(2,505);
-        twitter.postTweet(1,333);
-        twitter.postTweet(2,22);
-        twitter.postTweet(1,11);
-        twitter.postTweet(1,205);
-        twitter.postTweet(2,203);
-        twitter.postTweet(1,201);
-        twitter.postTweet(2,213);
-        twitter.postTweet(1,200);
-        twitter.postTweet(2,202);
-        twitter.postTweet(1,204);
-        twitter.postTweet(2,208);
-        twitter.postTweet(2,233);
-        twitter.postTweet(1,222);
-        twitter.postTweet(2,211);
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
 
-        System.out.println(twitter.getNewsFeed(1));
-        twitter.follow(1,2);
-        System.out.println(twitter.getNewsFeed(1));
-        twitter.unfollow(1,2);
-        System.out.println(twitter.getNewsFeed(1));
+public class Main {
+    public static void main(String[] args) {
+        Twitter twitter;
+        try {
+            twitter = Twitter.loadFromFile("data.ser");
+            System.out.println("成功載入資料");
+        } catch (IOException | ClassNotFoundException e) {
+            twitter = new Twitter();
+            System.out.println("無先前資料，使用新資料");
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("歡迎使用X");
+        while (true) {
+            System.out.println("輸入您的身份(整數):");
+            int id = sc.nextInt();
+            System.out.println("################");
+            System.out.println("[1]追蹤");
+            System.out.println("[2]解除追蹤");
+            System.out.println("[3]發文");
+            System.out.println("[4]讀取文章");
+            System.out.println("[5]讀取追蹤名單");
+            System.out.println("[0]結束程式");
+            System.out.println("################");
+            System.out.println("選擇功能：");
+            sc.nextLine();
+            String s = sc.nextLine();
+
+
+            switch (s) {
+                case "1":
+                    System.out.println("輸入要追蹤的對象");
+                    int follow = sc.nextInt();
+                    if (id == follow) {
+                        System.out.println("不能追蹤自己");
+                        break;
+                    }
+                    twitter.follow(id, follow);
+                    System.out.println("用戶id:" + id + "成功追蹤 用戶id:" + follow);
+                    break;
+
+                case "2":
+                    System.out.println("輸入解除追蹤的對象(整數)");
+                    int unfollow = sc.nextInt();
+                    try {
+                        twitter.unfollow(id, unfollow);
+                        System.out.println("用戶id:" + id + "以解除追蹤 用戶id:" + unfollow);
+                    } catch (NullPointerException e) {
+                        System.out.println("尚未追蹤此對象");
+                    }
+                    break;
+                case "3":
+                    Random r = new Random();
+
+                    int tweetId = r.nextInt();
+
+                    System.out.println("請輸入發文內容");
+                    String context = sc.nextLine();
+                    twitter.postTweet(id, tweetId, context);
+                    System.out.println("成功發文！");
+                    break;
+                case "4":
+                    System.out.println("Id:"+id+"的推文列表");
+                    try {
+                        System.out.println(twitter.getNewsFeed(id));
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("未有任何推文");
+                    }
+                    break;
+                case "5":
+                    try {
+                        System.out.println("用戶id:" + id + "追蹤清單:" + twitter.followerList(id).toString());
+                    } catch (Exception e) {
+                        System.out.println("尚未追蹤任何人");
+                    }
+                    break;
+                case "0":
+                    System.exit(0);
+                default:
+                    System.out.println("Unexpected value: " + s);
+            }
+            twitter.toSave();
+        }
+
 
     }
 }
--- src/Twitter.java
+++ src/Twitter.java
@@ -1,22 +1,38 @@
+import java.io.*;
 import java.util.*;
 
-class NewsAndTime {
+class NewsAndTime implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     int newsId;
     int time;
-    NewsAndTime(int newsId, int time){
+
+    public String getContext() {
+        return context;
+    }
+
+    private final String context;
+
+    NewsAndTime(int newsId, int time, String context) {
         this.newsId = newsId;
         this.time = time;
+        this.context = context;
     }
 }
 
-class Twitter {
+class Twitter implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     LinkedList<HashSet<Integer>> follower = new LinkedList<>();
     HashSet<Integer> followerInner = new HashSet<>();
-    HashMap<Integer,HashSet<Integer>> mapInner = new HashMap<>();
-    HashMap<Integer,LinkedList<NewsAndTime>> newsFeeds = new HashMap<>();
+    HashMap<Integer, HashSet<Integer>> mapInner = new HashMap<>();
+
+
+
+    HashMap<Integer, LinkedList<NewsAndTime>> newsFeeds = new HashMap<>();
     Map<Integer, LinkedList<NewsAndTime>> idToNewsInnerMap = new HashMap<>();
-    PriorityQueue<NewsAndTime> newsSorted = new PriorityQueue<>(
-            (n1, n2) -> n2.time - n1.time);
+    transient PriorityQueue<NewsAndTime> newsSorted = new PriorityQueue<>(
+            (n1, n2) -> n2.time - n1.time);//transient 不會被序列化
 
 
     public int getMAX() {
@@ -25,61 +41,82 @@ class Twitter {
 
     private final int MAX;
     private int time;
+
     public Twitter() {
         this.MAX = 10;
         this.time = 0;
     }
+
     LinkedList<Integer> init = new LinkedList<>();
 
-    public void postTweet(int userId, int tweetId) {
-        for(int i =0;i<=userId;i++)
+    public void postTweet(int userId, int tweetId, String context) {
+        for (int i = 0; i <= userId; i++)
             init.add(0);
-        if(init.get(userId)==0)
-            follow(userId,userId);
-        init.set(userId,1);
-        idToNewsInnerMap.computeIfAbsent(userId,k -> new LinkedList<>()).add(new NewsAndTime(tweetId,time++));
-        newsFeeds.put(userId,idToNewsInnerMap.get(userId));
+        if (init.get(userId) == 0)
+            follow(userId, userId);
+        init.set(userId, 1);
+        idToNewsInnerMap.computeIfAbsent(userId, k -> new LinkedList<>()).add(new NewsAndTime(tweetId, time++,context));
+        newsFeeds.put(userId, idToNewsInnerMap.get(userId));
     }
 
-public List<Integer> getNewsFeed(int userId) {
-    List<Integer> newsFeedList = new ArrayList<>();
+
+public List<String> getNewsFeed(int userId) {
+    List<String> newsFeedList = new ArrayList<>();
     newsSorted = new PriorityQueue<>(
             (n1, n2) -> n2.time - n1.time);
 
-    int count = 0;
-    if(follower.isEmpty())
-        return new LinkedList<>();
-    for (var i : follower.get(userId)) {
-        LinkedList<NewsAndTime> newsList = newsFeeds.get(i);
-        if (newsList != null) {
-            newsSorted.addAll(newsList);
+        int count = 0;
+        if (follower.isEmpty())
+            return new LinkedList<>();
+        for (var i : follower.get(userId)) {
+            LinkedList<NewsAndTime> newsList = newsFeeds.get(i);
+            if (newsList != null) {
+                newsSorted.addAll(newsList);
+            }
         }
-    }
 
-    while (!newsSorted.isEmpty() && count<getMAX()) {
-        newsFeedList.add(Objects.requireNonNull(newsSorted.poll()).newsId);
-        count++;
-    }
+        while (!newsSorted.isEmpty() && count < getMAX()) {
+            newsFeedList.add(Objects.requireNonNull(newsSorted.poll()).getContext());
+            count++;
+        }
 
-    return newsFeedList;
-}
+        return newsFeedList;
+    }
 
     public void follow(int followerId, int followeeId) {
-            followerInner = mapInner.getOrDefault(followerId,new HashSet<>());
-            followerInner.add(followeeId);
-            mapInner.put(followerId,followerInner);
-            if(followerId>=follower.size())
-                for (int i = 0; i < followerId+1; i++) {
-                    follower.add(new HashSet<>());
-                }
+        followerInner = mapInner.getOrDefault(followerId, new HashSet<>());
+        followerInner.add(followeeId);
+        mapInner.put(followerId, followerInner);
+        if (followerId >= follower.size())
+            for (int i = 0; i < followerId + 1; i++) {
+                follower.add(new HashSet<>());
+            }
 
-            follower.set(followerId, mapInner.get(followerId));//如果沒有會報錯
+        follower.set(followerId, mapInner.get(followerId));
     }
 
     public void unfollow(int followerId, int followeeId) {
-        if(followerId != followeeId) {
+        if (followerId != followeeId) {
             mapInner.get(followerId).remove(followeeId);
             follower.set(followerId, mapInner.get(followerId));
+        }
+    }
+    public HashSet<Integer> followerList(int id) {
+        return follower.get(id);
+    }
+    public void toSave()
+    {
+        try(FileOutputStream fos = new FileOutputStream("data.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static Twitter loadFromFile(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Twitter) ois.readObject();
         }
     }
 }
